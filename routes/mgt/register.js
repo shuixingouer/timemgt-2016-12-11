7745601f45dfb159a52efffc862349b1f077932a
shuixing <shuixing@ixiaopu.com>
2016-12-02 17:15:56 +0800

    刘同学的页面

--- routes/mgt/register.js
+++ routes/mgt/register.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 /* GET register page. */
 router.route("/").get(function(req,res){// 到达此路径则渲染register文件，并传出title值供 register.html使用
-  res.render("register",{title:'User Register'});
+  res.render("register",{title:'mgtUser Register'});
 }).post(function(req,res){
   var uname = req.body.uname;
   var upwd = req.body.upwd;
